51562bdedc6825551766d735360f00c9b39737d2
uuparupa <uparupa@uuparupa-no-MacBook-Air.local>
2019-10-15 07:34:01 +0900

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
@@ -10,15 +11,37 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule,MatMenuModule } from '@angular/material';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
 
+const appRoutes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'details',      component: DetailsComponent },
+ // {
+  //  path: 'heroes',
+  //  component: HeroListComponent,
+  //  data: { title: 'Heroes List' }
+ // },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+ // { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent
+    SidenavComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     LayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
